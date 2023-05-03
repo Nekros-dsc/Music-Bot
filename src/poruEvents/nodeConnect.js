6defe4cdddddbf8c8c84ba8a3d68a7fca5590bad
!"Nekros <122643715+Nekros-dsc@users.noreply.github.com>
2023-05-03 15:47:53 +0200

    Delete src directory

--- src/poruEvents/nodeConnect.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = (client, node) => {
-  console.log(`[NODE] ${node.name} a été télécharger avec succès`.blue);
-  console.log(`[Support] go join discord.gg/novaworld`.yellow);
-  console.log("----------------------------------------".white);
-};
--- src/poruEvents/queueEnd.js
+++
@@ -1,3 +0,0 @@
-module.exports.run = (client, player) => {
-    return player.destroy();
-};
